03786f2b41ba46671dec7666a903b8f104f76199
jibin <jibinkrishna0010@gmail.com>
2026-05-06 16:38:44 +0530

    registration and login functionality

--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render #type: ignore
+from django.shortcuts import render, redirect #type: ignore
 from blogs.models import Category, Blog
 from assignments.models import About
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm #type: ignore
+from django.contrib import auth #type: ignore
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
@@ -19,3 +22,39 @@ def home(request):
     }
     return render(request, 'home.html', contex)
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
